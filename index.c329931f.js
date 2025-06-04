7bb5e500abe90fb50c9a016dbcbacd77e8101577
Daria <darialys2510@gmail.com>
2025-06-04 20:22:20 +0200

    make build

---
+++ index.c329931f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table");e&&e.querySelectorAll("tr").forEach(function(e){var r=e.children[1];if(r){var l=r.cloneNode(!0),c=e.children[e.children.length-1];e.insertBefore(l,c)}});
+//# sourceMappingURL=index.c329931f.js.map
