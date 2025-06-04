0198dd6b471adc19b65115c87e2c299363a4850b
Daria <darialys2510@gmail.com>
2025-06-04 20:21:57 +0200

    add check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const table = document.querySelector('table');
-const rows = table.querySelectorAll('tr');
 
-rows.forEach((row) => {
-  const secondColumn = row.children[1];
-  const clonedColumn = secondColumn.cloneNode(true);
-  const lastColumn = row.children[row.children.length - 1];
+if (table) {
+  const rows = table.querySelectorAll('tr');
 
-  row.insertBefore(clonedColumn, lastColumn);
-});
+  rows.forEach((row) => {
+    const secondColumn = row.children[1];
 
-// write your code here
+    if (secondColumn) {
+      const clonedColumn = secondColumn.cloneNode(true);
+      const lastColumn = row.children[row.children.length - 1];
+
+      row.insertBefore(clonedColumn, lastColumn);
+    }
+  });
+}
